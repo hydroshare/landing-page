b10f991b1333ed75065f44f906cef427336142b0
sblack-usu <sblack@cuahsi.org>
2025-01-28 13:22:41 -0700

    strip out non-discovery work

--- api/discover/main.py
+++ api/discover/main.py
@@ -2,16 +2,11 @@ import os
 import subprocess
 
 import motor
-from beanie import init_beanie
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
-from subsetter.app.db import User, db
 from subsetter.app.routers.discovery import router as discovery_router
-from subsetter.app.routers.hydroshare import router as hydroshare_router
-from subsetter.app.schemas import UserRead, UserUpdate
-from subsetter.app.users import SECRET, auth_backend, cuahsi_oauth_client, fastapi_users
 from subsetter.config import get_settings
 
 # TODO: get oauth working with swagger/redoc
@@ -42,70 +37,14 @@ parent_dir = os.path.dirname(__file__)
 static_dir = os.path.join(parent_dir, "app/schemas")
 app.mount("/api/schemas", StaticFiles(directory=static_dir), name="schemas")
 
-
-app.include_router(
-    hydroshare_router,
-    # prefix="/auth/cuahsi",
-    tags=["hydroshare"],
-)
-
 app.include_router(
     discovery_router,
     prefix="/api/discovery",
     tags=["discovery"],
 )
 
-app.include_router(
-    fastapi_users.get_oauth_router(
-        cuahsi_oauth_client,
-        auth_backend,
-        SECRET,
-        redirect_url=get_settings().oauth2_redirect_url,
-        associate_by_email=True,
-    ),
-    prefix="/auth/cuahsi",
-    tags=["auth"],
-)
-
-app.include_router(
-    fastapi_users.get_oauth_router(
-        cuahsi_oauth_client,
-        auth_backend,
-        SECRET,
-        redirect_url=get_settings().vite_oauth2_redirect_url,
-        associate_by_email=True,
-    ),
-    prefix="/auth/front",
-    tags=["auth"],
-)
-
-app.include_router(
-    fastapi_users.get_users_router(UserRead, UserUpdate),
-    prefix="/users",
-    tags=["users"],
-)
-
-
 @app.on_event("startup")
 async def on_startup():
-    await init_beanie(
-        database=db,
-        document_models=[
-            User,
-        ],
-    )
-    arguments = [
-        'mc',
-        'alias',
-        'set',
-        'cuahsi',
-        f"https://{get_settings().minio_api_url}",
-        get_settings().minio_access_key,
-        get_settings().minio_secret_key,
-    ]
     app.db = motor.motor_asyncio.AsyncIOMotorClient(get_settings().mongo_url)
     app.mongodb = app.db[get_settings().mongo_database]
-    try:
-        _output = subprocess.check_output(arguments)
-    except subprocess.CalledProcessError as e:
-        raise
+
