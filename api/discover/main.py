f36cae84772cccfcdf22b02f026b1f8c3a69dfe8
sblack-usu <sblack@cuahsi.org>
2025-04-09 15:03:07 -0600

    remove corsmiddleware

--- api/discover/main.py
+++ api/discover/main.py
@@ -1,6 +1,4 @@
-from motor.motor_asyncio import AsyncIOMotorClient
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 #from discover.app.routers.discovery import router as discovery_router
 from discover.app.routers.pubsub import router as pubsub_router
@@ -13,32 +11,10 @@ from discover.config import get_settings
 # https://fastapi.tiangolo.com/how-to/configure-swagger-ui/
 # https://github.com/tiangolo/fastapi/pull/499
 
-app = FastAPI(servers=[{"url": get_settings().vite_app_api_url}])
-
-origins = [get_settings().allow_origins]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-#app.include_router(
-#    discovery_router,
-#    prefix="/api/discovery",
-#    tags=["discovery"],
-#)
+app = FastAPI()
 
 app.include_router(
     pubsub_router,
     prefix="/api/pubsub",
     tags=["pubsub"],
 )
-
-@app.on_event("startup")
-async def on_startup():
-    app.db = AsyncIOMotorClient(get_settings().mongo_url)
-    app.mongodb = app.db[get_settings().mongo_database]
-
