8403cb9e6756d06a7f01c12f20dc051b7d67f1e3
MSDrao <msandesh0055@gmail.com>
2025-07-18 10:32:42 -0500

    Remmmoved credentials

--- src/components/landing-page/cd.landing-page.vue
+++ src/components/landing-page/cd.landing-page.vue
@@ -242,7 +242,7 @@ class App extends Vue {
         endpoint: 'https://s3.hydroshare.org',
         forcePathStyle: true,
         credentials: {
-          accessKeyId: 'GET FROM_HS',
+          accessKeyId: 'GET_FROM_HS',
           secretAccessKey: 'GET_FROM_HS',
         },
       });
