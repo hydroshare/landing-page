5ce3733c65b5be2304d30c7096740eea148271e6
Devin Cowan <dcowan@cuahsi.org>
2025-09-16 12:18:17 -0400

    add hsUppy component for file uploads and remove legacy implementation

---
+++ src/components/landing-page/hs-uppy.vue
@@ -0,0 +1,82 @@
+<template>
+  <div id="uppy"></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, toNative } from "vue-facing-decorator";
+import Uppy from '@uppy/core';
+import Dashboard from '@uppy/dashboard';
+import AwsS3, { type AwsBody } from '@uppy/aws-s3';
+
+import '@uppy/core/css/style.min.css';
+import '@uppy/dashboard/css/style.min.css';
+
+@Component({
+  name: "hs-uppy",
+  components: {},
+})
+class HsUppy extends Vue {
+  mounted() {
+    const uppy = new Uppy({
+      id: "uppy",
+      // https://uppy.io/docs/uppy/#autoproceed
+      autoProceed: true,
+      // debug: true,
+      // https://uppy.io/docs/dashboard/#locale
+      locale: {
+        strings: {
+          browseFolders: 'upload a folder',
+          dropPasteImportBoth: 'Drop files here, %{browseFolders} or import from:',
+        },
+        pluralize: function (n: number): number {
+          throw new Error('Function not implemented.');
+        }
+      },
+    })
+    .use(Dashboard, {
+      inline: true,
+      fileManagerSelectionType: "both", // files and folders
+      target: "#uppy",
+      // showProgressDetails: true,
+      note: "TODO: quota note?",
+      // https://uppy.io/docs/dashboard/#locale
+    })
+
+    .use(AwsS3, { endpoint: 'https://localhost/companion/' })
+
+    //   uppy.use(GoogleDrivePicker, {
+    //     // https://uppy.io/docs/google-drive-picker/
+    //     target: Dashboard,
+    //     companionUrl: COMPANION_URL,
+    //     clientId: GOOGLE_PICKER_CLIENT_ID,
+    //     apiKey: GOOGLE_PICKER_API_KEY,
+    //     appId: GOOGLE_PICKER_APP_ID,
+    //   });
+    // }
+
+    // uppy.use(GoldenRetriever)
+    // on page load, check the uppy state to see if there were pending uploads
+    // https://uppy.io/docs/uppy/#getstate
+      
+    .on("error", (errorMessage) => {
+      let errorMsg = "";
+      if (typeof errorMessage === "object") {
+        for (const [key, value] of Object.entries(errorMessage)) {
+          errorMsg += `${key}: ${value}`;
+        }
+      } else {
+        errorMsg = JSON.stringify(errorMessage);
+      }
+      try {
+        let errorMessageJSON = JSON.parse(errorMessage.message);
+        if (errorMessageJSON.hasOwnProperty("validation_error")) {
+          errorMsg = errorMessageJSON.validation_error;
+        } else if (errorMessageJSON.hasOwnProperty("file_size_error")) {
+          errorMsg = errorMessageJSON.file_size_error;
+        }
+      } catch (e) {}
+    })
+  }
+}
+export default toNative(HsUppy);
+</script>
