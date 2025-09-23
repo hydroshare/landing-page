4abcedf85ad122cfaf96ee22d5b75a9a2c3c50d5
Devin Cowan <dcowan@cuahsi.org>
2025-09-23 13:03:07 -0400

    fix: remove leading slashes from image paths in HTML, Vue, and constants files

--- vite.config.ts
+++ vite.config.ts
@@ -74,17 +74,17 @@ export default defineConfig(({ mode }) => {
           theme_color: "#ffffff",
           icons: [
             {
-              src: "/pwa-192x192.png",
+              src: "pwa-192x192.png",
               sizes: "192x192",
               type: "image/png",
             },
             {
-              src: "/pwa-512x512.png",
+              src: "pwa-512x512.png",
               sizes: "512x512",
               type: "image/png",
             },
             {
-              src: "/pwa-512x512.png",
+              src: "pwa-512x512.png",
               sizes: "512x512",
               type: "image/png",
               purpose: "any maskable",
