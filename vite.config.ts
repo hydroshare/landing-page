f36b5a1dd65984d55165018356df32338845a087
Devin Cowan <dcowan@cuahsi.org>
2025-09-29 23:30:12 -0400

    refactor: remove custom asset handling from build configuration in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -125,22 +125,6 @@ export default defineConfig(({ mode }) => {
 
     build: {
       outDir: "./dist",
-      assetsDir: "assets",
-      rollupOptions: {
-        output: {
-          chunkFileNames: 'assets/js/[name]-[hash].js',
-          entryFileNames: 'assets/js/[name]-[hash].js',
-          assetFileNames: ({name}) => {
-            if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')) {
-              return 'assets/images/[name]-[hash][extname]';
-            }
-            if (/\.css$/.test(name ?? '')) {
-              return 'assets/css/[name]-[hash][extname]';
-            }
-            return 'assets/[name]-[hash][extname]';
-          },
-        }
-      }
     },
 
     server: {
