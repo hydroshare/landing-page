f8fcc77479ffacf1e830340e8aadfa2dd74707f9
Devin Cowan <dcowan@cuahsi.org>
2025-10-01 12:13:57 -0400

    fix: update Uppy AwsS3 endpoint to use COMPANION_URL

--- src/components/landing-page/hs-uppy.vue
+++ src/components/landing-page/hs-uppy.vue
@@ -143,8 +143,7 @@ class HsUppy extends Vue {
     uppyInstance.use(AwsS3, {
       headers: headers,
       allowedMetaFields: true,
-      endpoint: "https://localhost/companion",
-      // endpoint: COMPANION_URL,
+      endpoint: COMPANION_URL,
     })
     .on("error", (errorMessage) => {
       console.error("Uppy error:", errorMessage);
