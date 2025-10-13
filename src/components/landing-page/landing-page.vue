752dfa3e5eb44daaf46adc5bdd96d377195a6f1f
Devin Cowan <dcowan@cuahsi.org>
2025-10-13 12:11:31 -0400

    Revert "remove the mock resource"
    This reverts commit 021d5238e83ed303f04bb5c32f530d34966def24.

--- src/components/landing-page/landing-page.vue
+++ src/components/landing-page/landing-page.vue
@@ -858,6 +858,7 @@ import { useGoTo } from "vuetify";
 import { EnumCreativeWorkStatus } from "@/types";
 
 import CdSpatialCoverageMap from "@/components/search-results/cd.spatial-coverage-map.vue";
+import { mockResource } from "./resource.mock";
 
 @Component({
   components: { CzForm, CzFileExplorer, S3Form, CdSpatialCoverageMap },
@@ -1119,6 +1120,7 @@ class LandingPage extends Vue {
       // TODO:
       this.data = {
         ...this.data,
+        ...mockResource,
       };
       // @ts-expect-error The key property is generated when the component is initialized
       this.rootDirectory.children = resource.initialStructure;
